e85c162480adf14f9f9e263a1b0dfa69129d7a9a
efraimnabil <efraimefefraim@gmail.com>
2023-03-16 18:34:44 -0700

    4-puts.c.c

---
+++ 0x00-hello_world/4-puts.c.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Use the function puts to print
+ *
+ * Return: 0 means no errors.
+*/
+
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return 0;
+}
