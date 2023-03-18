cd5858b5a98b396d131ad1c1cbb30e6d44de542e
efraimnabil <efraimefefraim@gmail.com>
2023-03-17 18:09:44 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:
+ *
+ * Return: 0 means no errors.
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		putchar(i);
+	for (i = 79; i < 123; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
