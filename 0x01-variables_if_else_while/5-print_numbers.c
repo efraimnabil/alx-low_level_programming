b95a496d3354d27b5e868ceb0dd12075ac5e295b
efraimnabil <efraimefefraim@gmail.com>
2023-03-17 18:02:51 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:
+ *
+ * Return: 0 means no errors.
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 11; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
