fac8305330e36d12c8854b3cb52eeeebe43cdcb3
efraimnabil <efraimefefraim@gmail.com>
2023-03-17 17:58:12 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:
+ *
+ * Return: 0 means no errors.
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		if (c != 'e' || c != 'q')
+			putchr(c);
+	putchar('\n');
+	return (0);
+}
