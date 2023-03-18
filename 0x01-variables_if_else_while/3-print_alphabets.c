635bffcf77345f2f2c83c5700c66a3665091bdaf
efraimnabil <efraimefefraim@gmail.com>
2023-03-17 17:46:30 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print the alphapets
+ *
+ * Return: 0 means no errors.
+*/
+
+int main(void)
+{
+        char c;
+
+        for (c = 'a'; c <= 'z'; c++)
+                putchar(c);
+	for (c = 'A'; c <= 'Z'; c++)
+                putchar(c);
+        putchar('\n');
+        return (0);
+}
