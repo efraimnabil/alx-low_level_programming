09ebcd1999eda4e494be1e7bfc8c7b3c9539ef35
efraimnabil <efraimefefraim@gmail.com>
2023-03-21 17:40:34 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include main.h
+
+/**
+ * main - Entry point
+ *
+ * Description:
+ *
+ * Return: 0 means no errors.
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+	return (0);
+}
