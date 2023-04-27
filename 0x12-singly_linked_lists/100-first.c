7be73ba3219362f1251e091115ef9a94a789ddbf
Efraim Nabil <efraimefefraim@gmail.com>
2023-04-27 19:12:01 +0200

    first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,11 +6,11 @@ void print_first(void) __attribute__ ((constructor));
  * print_first - Prints a string before the main function is executed.
  *
  * Description:
- *  - The function prints a string to stdout before the main function is executed.
+ *  - function prints a string to stdout before the main function is executed.
  *  - The function does not return anything.
  */
 void print_first(void)
 {
-	printf("You're beat! and yet, you must allow,\n"
-	       "I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
