48516277fea218674654dfb7af3107803de6ccf5
Efraim Nabil <efraimefefraim@gmail.com>
2023-04-27 18:55:44 +0200

    add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,30 +1,34 @@
-#include <stdlib.h>
-#include <string.h>
 #include "lists.h"
 
 /**
- * add_node - adds a new node at the beginning of a linked list
- * @head: double pointer to the list_t list
- * @str: new string to add in the node
+ * add_node - Adds a new node at the beginning of a list_t list.
+ * @head: A pointer to the head of the list_t list.
+ * @str: The string to be added to the list_t list.
  *
- * Return: the address of the new element, or NULL if it fails
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new element.
+ *
+ * Description:
+ *  - str is duplicated.
+ *  - the function strndup from 0x0B-malloc_free.c is used.
  */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	unsigned int len = 0;
-
-	while (str[len])
-		len++;
+	unsigned int len;
 
 	new = malloc(sizeof(list_t));
-	if (!new)
+	if (new == NULL)
 		return (NULL);
+
+	for (len = 0; str[len];)
+		len++;
 
 	new->str = strdup(str);
 	new->len = len;
-	new->next = (*head);
-	(*head) = new;
+	new->next = *head;
 
-	return (*head);
+	*head = new;
+
+	return (new);
 }
