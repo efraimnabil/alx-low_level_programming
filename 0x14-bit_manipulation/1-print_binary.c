308e972ea68883f5645cc2f0a266c485bad16654
Efraim Nabil <efraimefefraim@gmail.com>
2023-05-04 16:47:55 +0300

    to binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,12 +9,19 @@
 
 void print_binary(unsigned long int n)
 {
-	while (n > 0)
+	if (n == 0)
 	{
-		if (n & 1)
-			_putchar('1');
-		else
-			_putchar('0');
-		n >>= 1;
+		_putchar('0');
+		return;
+	}
+	else if (n == 1)
+	{
+		_putchar('1');
+		return;
+	}
+	else
+	{
+		print_binary(n >> 1);
+		_putchar((n & 1) + '0');
 	}
 }
