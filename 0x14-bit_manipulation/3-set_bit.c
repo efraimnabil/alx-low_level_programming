bc27c98c665108ee41052ce21ac7ceb5f4bf32bf
Efraim Nabil <efraimefefraim@gmail.com>
2023-05-04 17:05:26 +0300

    set bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,13 +10,13 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-    unsigned long int mask = 1;
+	unsigned long int mask = 1;
 
-    if (index > 63)
-        return (-1);
+	if (index > 63)
+		return (-1);
 
-    mask <<= index;
-    *n = *n | mask;
+	mask <<= index;
+	*n = *n | mask;
 
-    return (1);
+	return (1);
 }
